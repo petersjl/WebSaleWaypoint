b3e2e732f91b785db174b09aafc24dce70df0b0e
Cooper Anderson <cooper@cooperstuff.ninja>
2020-10-09 23:59:11 -0500

    Remove all React and replace with layman code

---
+++ public/src/scripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview Main script for all pages
+ * @author Cooper Anderson (andersc7), Joseph Peters (petersjl)
+ */
+
+/**
+ * This line is so that we do not have squiggly lines under every instance of the word "firebase"
+ */
+firebase = firebase || {};
+
+firebase.firestore()
